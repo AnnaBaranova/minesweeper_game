bab4509341822736fe93643a9b11bb28ad855ac2
Anna Baranova <asbaranova1@gmail.com>
2021-03-27 18:12:48 -0400

    add createCell function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 const boardContainer = $("#board-container")
 
-const 
+function createCell() {
+    for (i = 0; i < 3; i++) {
+        const newRow = $(`<div class="row" id="row-${i}"></div>`);
+        for (y = 0; y < 3; y++) {
+            const newCell = $(`<div class="col" id="${i}-${y}">col</div>`);
+            newRow.append(newCell);
+        }
+        boardContainer.append(newRow);
+    }
+}
+
+createCell();
