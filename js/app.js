1863a40c9aef89a40b392c4d233c4016f4dd303e
Anna Baranova <asbaranova1@gmail.com>
2021-03-29 21:22:00 -0400

    add function generateMines

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         name: "easy",
         row: 10,
         column: 10,
-        mine: 5,
+        mine: 10,
     },
     board: [],
     mineLeft: 0,
@@ -26,6 +26,7 @@ const boardContainer = $("#board-container")
 
 
 function initGame (){
+    // function create 2D array board
     for (i = 0; i < game.level.row; i++) {
         const newRow = [];
         for (y = 0; y < game.level.column; y++) {
@@ -40,7 +41,19 @@ function initGame (){
         }
         game.board.push(newRow);
     }
+    game.gameOver = false;
+    
+    // function generateMines
+    while (game.mineLeft < game.level.mine) {
+        const randomRow = Math.floor(Math.random()*game.level.row);
+        const randomColumn = Math.floor(Math.random()*game.level.column);
+        if (!game.board[randomRow][randomColumn].hasMine) {
+            game.board[randomRow][randomColumn].hasMine = true;
+            game.mineLeft++;
+        }
+    }
     console.log (game.board);
+    console.log(game.mineLeft);
 }
 
 initGame();
@@ -49,7 +62,7 @@ function createCell() {
     for (i = 0; i < game.level.row; i++) {
         const newRow = $(`<div class="row" id="row-${i}"></div>`);
         for (y = 0; y < game.level.column; y++) {
-            const newCell = $(`<div class="col" id="${i}-${y}">col</div>`);
+            const newCell = $(`<div class="col" id="${i}-${y}">${game.board[i][y].hasMine}</div>`);
             newRow.append(newCell);
         }
         boardContainer.append(newRow);
